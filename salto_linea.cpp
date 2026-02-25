3bc105a2dfccb42bd93a545ccc6bcddc7c65ed23
Christian Morales <camorales@estud.usfq.edu.ec>
2026-02-25 10:13:45 -0500

    salto_linea

---
+++ salto_linea.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string sustantivo;
+string verbo;
+   cout << "Ingrese un sustantivo:  ";
+   cin >>sustantivo;
+
+    cout << "Ingrese un verbo:  ";
+    cin >>verbo;
+
+    cout << "!El " << sustantivo << " decidio " << verbo << "en la luna"<< endl;
+
+    return 0;
+}
